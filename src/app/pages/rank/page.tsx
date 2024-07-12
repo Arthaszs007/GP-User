1b5dc22125141c4d10e050e0eaf7fadc2f0f0e51
Arthas007 <arthaszs007@gmail.com>
2024-07-11 23:01:38 -0600

    update

--- src/app/pages/rank/page.tsx
+++ src/app/pages/rank/page.tsx
@@ -34,21 +34,13 @@ const Rank = () => {
   return (
     <div>
       {session ? (
-        <div className="flex flex-col items-center">
-          <p className="text-4xl text-center mt-4">Rank page is coming soon</p>
-          <img
-            className="h-[30rem] mt-3 w-[60rem]"
-            src="https://mastersenseigenetics.com/wp-content/uploads/2021/04/UnderConstruction.jpeg"
-            alt="default"
-          />
-        </div>
-      ) : (
-        // <p className="text-4xl text-center">Please sign in to view rank page</p>
         <div className="flex flex-wrap">
           {ranks?.map((item, index) => (
             <RankListChild rank={item} ids={item.children} key={index} />
           ))}
         </div>
+      ) : (
+        <p className="text-4xl text-center">Please sign in to view rank page</p>
       )}
     </div>
   );
